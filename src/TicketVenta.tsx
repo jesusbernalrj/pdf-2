3c575e13e61820bced88bba0e8a87ca29b5afe20
jesusbernalrj <130814444+jesusbernalrj@users.noreply.github.com>
2024-04-16 10:56:05 -0700

    Add files via upload

---
+++ src/TicketVenta.tsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+
+import ticket from './App';
+export interface Response {
+  success: string,
+  message: string,
+  content: string
+}
+export default function App() {
+  const [base64, setBase64] = useState('');
+  const [message, setMessage] = useState('');
+
+  const onGenerateTicket = async (output: string) => {
+    setBase64('');
+    setMessage('');
+
+    const response: Response = await ticket(output, '');
+
+    if (!response?.success) {
+      alert(response?.message);
+      return;
+    }
+
+    if (output === 'b64') {
+      setBase64(response?.content ?? '');
+    }
+
+    setMessage(response?.message);
+
+    setTimeout(() => {
+      setMessage('');
+    }, 2000);
+  };
+
+  return (
+    <div className="mx-main-container">
+      <div className="mx-btn-container">
+        <button
+          className="mx-btn-primary"
+          onClick={() => onGenerateTicket('b64')}
+        >
+          GENERAR TICKET
+        </button>
+        <button
+          className="mx-btn-secondary"
+          onClick={() => onGenerateTicket('print')}
+        >
+          IMPRIMIR TICKET
+        </button>
+      </div>
+      {message && <p className="mx-alert-info">{message}</p>}
+      {base64 && (
+        <iframe
+          src={`data:application/pdf;base64,${base64}`}
+          className="mx-iframe"
+        />
+      )}
+    </div>
+  );
+}
---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import TicketComponent from './TicketVenta'
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <TicketComponent/>
+  </React.StrictMode>,
+)
